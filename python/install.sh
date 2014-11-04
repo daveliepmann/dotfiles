e141a1bdea56772b6c7dd8eefcf385292968be00
Daniel Barreto <dbarreto@talpor.com>
2014-11-03 20:08:11 -0430

    Adds python install.sh.
    Installs virtualenv/virtualenvwrapper.

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,6 @@
-easy_install SpoofMAC
+#!/usr/bin/env bash
+
+wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.11.6.tar.gz#md5=f61cdd983d2c4e6aeabb70b1060d6f49
+tar -xvzf virtualenv-1.11.6.tar.gz
+python virtualenv-1.11.6/virtualenv.py ~/.python
+~/.python/bin/pip install virtualenv virtualenvwrapper
