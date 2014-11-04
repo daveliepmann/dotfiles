dc9715aeb71a5f0a31ee782124fd6c9e1c6bbd8f
Daniel Barreto <dbarreto@talpor.com>
2014-11-03 20:37:06 -0430

    Adds some new zsh aliases and config.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,5 @@
 alias reload!='. ~/.zshrc'
+alias dv='dirs -v | head -10'
+unalias rm
+unalias cp
+unalias mv
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,48 +1,34 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
+bindkey -e
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
+#-------------------------------------------------------------------------------
+# History
+export HISTFILE=~/.long_history
+export HISTFILESIZE=50000
+export HISTSIZE=100000
+export SAVEHIST=100000
+export HISTCONTROL=ignoredups
+export HISTTIMEFORMAT="%h/%d - %H:%M:%S"
 
-fpath=($ZSH/functions $fpath)
-
-autoload -U $ZSH/functions/*(:t)
-
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
-
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_FCNTL_LOCK
+setopt EXTENDED_HISTORY
 setopt HIST_REDUCE_BLANKS
+setopt NO_SHARE_HISTORY
+unsetopt correct_all
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
+bindkey '\e[5~' history-substring-search-up
+bindkey '\e[6~' history-substring-search-down
+bindkey "^R" history-incremental-pattern-search-backward
+bindkey "^S" history-incremental-pattern-search-forward
 
-zle -N newtab
+#---------------------------------------------------------------------
+# python
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+export DJANGO_COLORS="dark"
+export VIRTUALENVWRAPPER_PYTHON=${HOME}/.python/bin/python
+export VIRTUALENVWRAPPER_VIRTUALENV=${HOME}/.python/bin/virtualenv
+. ${HOME}/.python/bin/virtualenvwrapper.sh
+
+# ------------------------------------------------------------------------------
+# Other
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
